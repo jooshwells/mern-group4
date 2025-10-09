03a2a054cccad99accddcb2395c26351e14394aa
jooshwells <joshwells637@gmail.com>
2025-10-09 09:43:26 -0400

    Quick test to see if this polling change breaks anything on Mac

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This is the important part
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Listen on all addresses, good for Docker
+    strictPort: true, // Optional: makes sure vite always uses the port you want
+  }
 })
