dc61acb022fecaaaa6405c75a29f90a32ef8b97d
Amos <amos7perez@gmail.com>
2025-10-12 14:41:39 -0400

    implement email verification

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,12 +1,33 @@
 import { normalize_system_error_response } from "../../custom.middleware.js";
 import User from "./auth.model.js";
 import jsonwebtoken from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import Handlebars from "handlebars";
+import fs from "fs";
+
+// export const register_user = async (request, response, next) => {
+//     try {
+//         const { first_name, last_name, email, password } = request.body;
+//         const user = new User({ first_name, last_name, email, password });
+//         await user.save();
+//         return response.success({ user: { first_name, last_name, email } }, "User registered!", 201);
+//     }
+//     catch (error)
+//     {
+//         return normalize_system_error_response(error);
+//     }
+// }
 
 export const register_user = async (request, response, next) => {
     try {
         const { first_name, last_name, email, password } = request.body;
         const user = new User({ first_name, last_name, email, password });
         await user.save();
+
+        request.user = {email: user.email};
+
+        await send_verification_email(request, response);
+
         return response.success({ user: { first_name, last_name, email } }, "User registered!", 201);
     }
     catch (error)
@@ -49,5 +70,51 @@ export const get_user = async (request, response) => {
     const { email } = request.user;
     const user = await User.findOne({ email });
 
-    return response.success({ user: { id: user._id, first_name: user.first_name, last_name: user.last_name, email: user.email } }, "User authenticated!", 200);
+    return response.success({ user: { id: user._id, first_name: user.first_name, last_name: user.last_name, email: user.email, is_verified: user.is_verified } }, "User authenticated!", 200);
+}
+
+export const verify_email = async (request, response) => {
+    request.user.is_verified = true;
+    request.user.verification_token = null;
+    await request.user.save();
+    return response.success({}, "User email verified", 200);
+}
+
+export const send_verification_email = async (request, response) => {
+    try {
+        const { email } = request.user;
+        const user = await User.findOne({ email });
+
+        const { first_name, last_name } = user;
+
+        const verification_token = jsonwebtoken.sign({ type: "email_verification", user_id: user._id, email: user.email }, process.env.JWT_SECRET, {expiresIn: '12h'});
+
+        user.verification_token = verification_token;
+        user.save();
+
+        const verification_link = process.env.APP_DOMAIN + "/api/auth/verify-email/" + verification_token;
+
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        const email_source = fs.readFileSync("./src/modules/auth/emails/verification-email.html", "utf8");
+        const email_template = Handlebars.compile(email_source);
+        const html = email_template({ name: first_name, verification_link: verification_link });
+
+        const info = await transporter.sendMail({
+            from: `${process.env.MAIL_FROM_NAME} <${process.env.MAIL_FROM}>`,
+            to: `${user.email}`,
+            subject: "Nanta Verification Email",
+            html: html,
+        });
+    } catch (error) {
+        return normalize_system_error_response(error, response);
+    }
 }
--- backend/src/modules/auth/auth.model.js
+++ backend/src/modules/auth/auth.model.js
@@ -5,7 +5,9 @@ const user_schema = new mongoose.Schema(
         first_name: { type: String, required: true },
         last_name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true }
+        password: { type: String, required: true },
+        is_verified: { type: Boolean, default: false },
+        verification_token: { type: String, default: null }
     },
     {
         timestamps: {
