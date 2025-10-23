113da38a0de1825d7e940b73aebcd430fa8d8150
HotdoggodtoH <nathan.ntnguyen@gmail.com>
2025-10-22 22:24:10 -0400

    Minor visual updates

--- frontend/src/components/MenuSearch.tsx
+++ frontend/src/components/MenuSearch.tsx
@@ -7,7 +7,7 @@ export default function MenuSearch({ children }: { children?: React.ReactNode })
     <div className="h-[85%] relative flex border border-amber-300 w-[240px] rounded-lg justify-center items-start pt-2 bg-background">
       <form className="flex w-full justify-center">
         <Input
-          className="w-[90%] h-[3%] bg-transparent border border-amber-300 rounded-lg text-center placeholder:text-muted-foreground text-current text-sm p-0"
+          className="w-[90%] h-10 bg-transparent border border-amber-300 rounded-lg text-center placeholder:text-muted-foreground text-current text-sm p-0"
           placeholder="Search"
           aria-label="Search"
         />
