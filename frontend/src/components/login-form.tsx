6ccf2b3f42dd015a4b81d1bd859fa30c52671304
Amos <amos7perez@gmail.com>
2025-10-29 21:11:29 -0400

    Login | Registration | Vite config dev env tweak

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -9,31 +9,63 @@ import {
 } from "@/components/ui/card"
 import {
   Field,
-  // FieldDescription, // not used right now, add back later if needed
+  FieldDescription,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { useNavigate } from "react-router-dom"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const navigate = useNavigate()
+
+  const handle_submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData.entries())
+  
+    try {
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      })
+  
+      console.log(res);
+
+      if (res.ok) {
+          navigate("/notebook");
+      }
+    } catch (err) {
+      console.error("Server error. Please try again later")
+    }
+  }
+
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="text-center">
+        <h1 className="font-bold text-5xl">NANTA</h1>
+        <p><span className="hidden">Oh, God, please!</span>not another note taking app</p>
+      </div>
       <Card>
-        <CardHeader>
+        <CardHeader className="text-center">
           <CardTitle>Login to your account</CardTitle>
           <CardDescription>
             Enter your email below to login to your account
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handle_submit}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
                 <Input
+                  name="email"
                   id="email"
                   type="email"
                   placeholder="jdoe@example.com"
@@ -50,7 +82,7 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input name="password" id="password" type="password" required />
               </Field>
               <Field>
                 <Button type="submit">Login</Button>
