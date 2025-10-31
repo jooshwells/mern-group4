3c7d1f3b7b6d88244a1364ff5c56d24a2bbc9d3a
Jayden A <jaurelus12@gmail.com>
2025-10-30 23:28:39 -0400

    Layout to edit pw

--- frontend/src/components/EditAccount.tsx
+++ frontend/src/components/EditAccount.tsx
@@ -1,5 +1,5 @@
 import { Button } from "./ui/button";
-
+import { useState } from "react";
 //
 function EditAccount() {
   /*When Change Password is pressed, make the button go away and make the user confirm current pw
@@ -7,13 +7,17 @@ function EditAccount() {
     Confirm the change 
     */
 
-  function changePW() {
+  const [hide, setVisible] = useState(false);
+
+  const changePW = () => {
+    setVisible((prev) => !prev);
     <p> Please enter your current password</p>;
-  }
+  };
+
   return (
     <div className="justify-center w-full">
-      <p className="text-center">Edit Account</p>
-      <div className="mt-20 flex flex-row">
+      <p className="text-center font-bold">Edit Account</p>
+      <div className="mt-10 flex flex-row">
         <div className="">
           <p className="mb-5">Username: </p>
           <p>Email: </p>
@@ -29,8 +33,31 @@ function EditAccount() {
           ></input>
         </div>
       </div>
-      <div className="flex justify-center">
-        <Button className="mt-5">Change Password</Button>
+      <div className="flex flex-col justify-center">
+        <Button
+          className="mt-5 bg-black border border-amber-400 text-red-700"
+          onClick={changePW}
+        >
+          Change Password
+        </Button>
+        <input
+          placeholder="Old Password"
+          className={`border border-amber-300 text-center mt-5 rounded-4xl ${
+            hide ? "block" : "hidden"
+          }`}
+        ></input>
+        <input
+          placeholder="New Password"
+          className={`border border-amber-300 text-center mt-5 rounded-4xl ${
+            hide ? "block" : "hidden"
+          }`}
+        ></input>
+        <input
+          placeholder="Confirm Password"
+          className={` border border-amber-300 text-center mt-5 rounded-4xl ${
+            hide ? "block" : "hidden"
+          }`}
+        ></input>
       </div>
     </div>
   );
