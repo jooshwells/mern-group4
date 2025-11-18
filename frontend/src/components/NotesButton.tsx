c6fcef2a8701b0bb817cd6b2cf8a59fc9e07720b
Jayden A <jaurelus12@gmail.com>
2025-11-18 12:37:55 -0500

    Fixing redirect path

--- frontend/src/components/NotesButton.tsx
+++ frontend/src/components/NotesButton.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 import { NotebookPen } from "lucide-react";
 
 export function NotesButton() {
+  const navigate = useNavigate();
+
+  const redirectNotes = () => {
+    navigate("/edit");
+  };
+
   return (
     <div>
-      <Button variant={"ghost"}>
+      <Button variant={"ghost"} onClick={redirectNotes}>
         <NotebookPen className="scale-200" />
       </Button>
     </div>
